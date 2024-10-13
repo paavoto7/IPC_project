3ceb21a6664b35960cdf2ef4ae4379220171857d
Paavo Westerlund <paawes@utu.fi>
2024-10-13 20:31:18 +0300

    Slight improvements and sorting

--- main.cpp
+++ main.cpp
@@ -19,13 +19,16 @@ int randGen() {
 int childFunc(int fd[]) {
     int randNum = randGen();
     write(fd[1], &randNum, sizeof(randNum));
+    close(fd[1]);
     cout << "Process: " << getpid() << ", number: " << randNum << endl;
-    return 0;
+    exit(0);
 }
 
 int main() {
 
     cout << "Main process pid: " << getpid() << endl << endl;
+
+    pid_t childPids[4];
 
     int fd[2];
     pipe(fd);
@@ -38,13 +41,18 @@ int main() {
     // Creates the P0-P4 processes
     for (int i = 0; i < 4; ++i) {
         pid_t pid = fork();
-
-        // Waits for the child to be done
-        if (pid != 0) {
-            wait(NULL);
-        } else if (pid == 0) {
-            return childFunc(fd);
+        childPids[i] = pid;
+        
+        if (pid == 0) {
+            childFunc(fd);
         }
+    }
+
+    close(fd[1]);
+    
+    // Waits for the child to be done
+    for (int i = 0; i < 4; ++i) {
+        waitpid(childPids[i], NULL, 0);
     }
 
     cout << endl;
@@ -52,10 +60,11 @@ int main() {
     int n;
     for (int i = 0; i < 4; ++i) {
         read(fd[0], &n, sizeof(n)); // Read pipe
-        int addr = i * sizeof(n); // Address to write (avoid segfault)
-        shmAdr[addr] = n; // shmAdr is a pointer so writing there
+        shmAdr[i] = n; // Write to the shared memory
         cout << "Reading from pipe: " << n << endl;
     }
+
+    close(fd[0]);
 
     cout << endl;
 
--- scheduler.cpp
+++ scheduler.cpp
@@ -14,20 +14,49 @@ executables as entrypoints that exchanged data through pipe/FIFO.
 This implementation seems to be working though so I suppose we're on the right tracks at least.
 */
 
+void sort(int nums[]) {
+    // Simple and easy bubble sort
+
+    bool swapped;
+
+    do {
+        swapped = false;
+        for (int i = 0; i < 3; ++i) {
+            if (nums[i] > nums[i+1]) {
+                int temp = nums[i+1];
+                nums[i+1] = nums[i];
+                nums[i] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
+
 int main() {
 
     key_t key = ftok("topSec", 1);
 
     int shmid = shmget(key, 4*sizeof(int), IPC_CREAT | 0666);
 
-    sleep(1);
+    sleep(1); // Or implement busy waiting??
 
     int* shmAdr = (int*) shmat(shmid, NULL, 0);
     
-    cout << "The numbers written are ";
+    cout << "The number from shared memory are: ";
+
+    int nums[4];
 
     for (int i = 0; i < 4; ++i) {
-        cout << shmAdr[i*sizeof(int)] << " ";
+        int num = shmAdr[i];
+        cout << num << " ";
+        nums[i] = num;
+    }
+
+    cout << endl << "The sorted numbers are: ";
+    
+    sort(nums);
+    for (int i = 0; i < 4; ++i) {
+        cout << nums[i] << " ";
     }
 
     cout << endl;
